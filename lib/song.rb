ec0bad0e8aada8ddf8c2f49a738fe577a4f71b85
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-04-15 22:26:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = []
+  @@artists = []
+  @@genres = []
+   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  
+end
